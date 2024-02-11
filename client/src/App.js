a4c8f06f28298de7f79dabdf2ad28f8f1b6f7d4a
IT21031120 <it21031120@my.sliit.lk>
2024-02-11 20:56:07 +0530

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBarView from './components/NavBarView';
+
+function App() {
+  return (
+    <>
+      <NavBarView />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBarView.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Tooltip from '@mui/material/Tooltip';
+import logo from '../images/yummy.png'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+function NavBarView() {
+    return (
+        <>
+            <AppBar position="fixed" sx={{ backgroundColor: 'white', }}>
+                <Container maxWidth="100%">
+                    <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Box sx={{ /* backgroundColor: 'blue', */ height: '42px', display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+                            <a href='/'>
+                                <img
+                                    src={logo}
+                                    width='70px'
+                                    height='auto'
+                                />
+                            </a>
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                component="a"
+                                href="/"
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: 'none', md: 'contents' },
+                                    flexGrow: 1,
+                                    fontFamily: 'Protest Guerrilla, sans-serif',
+                                    fontWeight: 700,
+                                    fontSize: '25px',
+                                    letterSpacing: '.1rem',
+                                    color: 'black',
+                                    textDecoration: 'none',
+                                    textTransform: 'uppercase',
+                                    marginLeft: '5px',
+
+                                }}
+                            >
+                                yummydish
+                            </Typography>
+                        </Box>
+                        <Box sx={{ flexGrow: 0, }}>
+                            <Tooltip title="Wishlist">
+                                <IconButton /* onClick={handleOpenUserMenu}  */ sx={{ p: 1 }}>
+                                    <FavoriteBorderIcon sx={{ color: 'black' }} />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Cart">
+                                <IconButton /* onClick={handleOpenUserMenu}  */ sx={{ p: 1 }}>
+                                    <ShoppingCartIcon sx={{ color: 'black' }} />
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar >
+        </>
+    )
+}
+
+export default NavBarView
